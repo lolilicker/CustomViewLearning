cc5d99360dfaa2f345347f589b342f3b9661908d
wangbo <644816416@qq.com>
2016-12-14 11:20:34 +0800

    加动画

--- app/src/main/java/com/okoer/customviewlearning/widget/Radar.java
+++ app/src/main/java/com/okoer/customviewlearning/widget/Radar.java
@@ -1,5 +1,6 @@
 package com.okoer.customviewlearning.widget;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -7,6 +8,7 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Point;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 import com.okoer.customviewlearning.DpAndPx;
@@ -16,15 +18,25 @@ import com.okoer.customviewlearning.DpAndPx;
  */
 
 public class Radar extends View {
-    private static final int RADAR_MARGIN = DpAndPx.Dp2Px(10);
+    private static final String TAG = "Radar";
+
+    private static final int RADAR_MARGIN = DpAndPx.Dp2Px(20);
+    //动画时长
+    private static final int ANIM_DURATION = 1000;
     private int radius;
-    private static final float data[] = {110, 120, 140, 130, 132};
+    private static final float data[] = {6.3f, 7.2f, 8.8f, 4.1f, 9.0f};
+    private static final String title[] = {"功效", "价格", "品牌", "外观", "耐用"};
     //每个角的弧度
     private static final float RADIAN = (float) ((Math.PI * 2) / data.length);
-    private static final int MAX_DATA = 150;
+    private static final int MAX_DATA = 10;
+    //标题大小
+    private static final float TITLE_TEXT_SIZE = 16;
 
     private Paint borderPaint;
     private Paint areaPaint;
+    private Paint titlePaint;
+
+    private float percent = 0;
 
     public Radar(Context context) {
         this(context, null);
@@ -42,20 +54,37 @@ public class Radar extends View {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        radius = Math.min(w, h) / 2;
-        postInvalidate();
+        radius = Math.min(w, h) / 3;
     }
 
     private void init() {
         borderPaint = new Paint();
         borderPaint.setColor(Color.WHITE);
         borderPaint.setStyle(Paint.Style.STROKE);
-        borderPaint.setStrokeWidth(2);
+        borderPaint.setStrokeWidth(DpAndPx.Dp2Px(2));
 
         areaPaint = new Paint();
         areaPaint.setColor(Color.WHITE);
         areaPaint.setAlpha(100);
         areaPaint.setStyle(Paint.Style.FILL);
+
+        titlePaint = new Paint();
+        titlePaint.setColor(Color.WHITE);
+        titlePaint.setStyle(Paint.Style.STROKE);
+        titlePaint.setTextSize(DpAndPx.sp2px(getContext(), TITLE_TEXT_SIZE));
+        titlePaint.setTextAlign(Paint.Align.CENTER);
+
+        ValueAnimator valueAnimator = ValueAnimator.ofFloat(1);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                percent = (float) valueAnimator.getAnimatedValue();
+                Log.d(TAG, "anim " + percent);
+                invalidate();
+            }
+        });
+        valueAnimator.setDuration(ANIM_DURATION);
+        valueAnimator.start();
     }
 
     @Override
@@ -89,17 +118,32 @@ public class Radar extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.drawColor(Color.BLUE);
-        canvas.translate(radius, radius);
+        canvas.translate(canvas.getWidth() / 2, canvas.getHeight() / 2);
         drawEdge(canvas);
         drawLine(canvas);
         drawArea(canvas);
+        drawTitle(canvas);
     }
 
-    private void drawArea(Canvas canvas) {
-        Path path = new Path();
+    private void drawTitle(Canvas canvas) {
+        Point point;
+        for (int i = 0; i < data.length; i++) {
+            point = getPoint(i, RADAR_MARGIN, 1);
+            Paint.FontMetrics fontMetrics = titlePaint.getFontMetrics();
+            float fontTotalHeight = fontMetrics.bottom - fontMetrics.top;
+            float offY = fontTotalHeight / 2 - fontMetrics.bottom;
+            float newY = point.y + offY;
+            canvas.drawText(title[i], point.x, newY, titlePaint);
+//            canvas.drawLine(0,0,point.x, newY,titlePaint);
+        }
+    }
+
+    private void drawArea(final Canvas canvas) {
+        final Path path = new Path();
+
         Point point = null;
         for (int i = 0; i < data.length; i++) {
-            point = getPoint(i, 0, data[i] / MAX_DATA);
+            point = getPoint(i, 0, data[i] / MAX_DATA * percent);
             if (i == 0) {
                 path.moveTo(point.x, point.y);
             } else {
@@ -108,6 +152,9 @@ public class Radar extends View {
         }
         path.close();
         canvas.drawPath(path, areaPaint);
+
+
+
     }
 
     private void drawLine(Canvas canvas) {
@@ -139,7 +186,7 @@ public class Radar extends View {
         switch (position) {
             case 0:
                 x = 0;
-                y = -radius + radarMargin;
+                y = -radius - radarMargin;
                 break;
             case 1:
                 x = (int) (Math.sin(RADIAN) * (radius + radarMargin));
